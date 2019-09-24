b85adf19d9cffd5ecfd85a2683186e2d19c682d8
saber2pr <saber2pr@gmail.com>
2019-09-24 16:05:02 +0000

    refactor(parse): update

--- src/core/parser.ts
+++ src/core/parser.ts
@@ -2,7 +2,7 @@
  * @Author: saber2pr
  * @Date: 2019-09-23 13:32:48
  * @Last Modified by: saber2pr
- * @Last Modified time: 2019-09-24 15:51:02
+ * @Last Modified time: 2019-09-24 16:04:17
  */
 export const count = (
   str: string,
@@ -19,13 +19,34 @@ export const count = (
     [0, false]
   )[0]
 
-const countWS = (line: string) => count(line, " ", ch => ch !== " ")
+export const countWS = (line: string) => count(line, " ", ch => ch !== " ")
+
+export const setMeta = <T>(target: T, ...metas: [string, any][]): T =>
+  Object.defineProperties(
+    target,
+    metas.reduce<PropertyDescriptorMap>(
+      (acc, [k, v]) => ({
+        ...acc,
+        [k]: { value: v, enumerable: false }
+      }),
+      {}
+    )
+  )
 
 export interface Node {
   name: string
   children?: this[]
   parent?: this
   tab?: number
+}
+
+export const getParent = (node: Node, nodeTab: number) => {
+  let tab = node.tab
+  while (tab >= nodeTab) {
+    tab = node.tab
+    node = node.parent
+  }
+  return node
 }
 
 export const parse = <T extends Node>(
@@ -38,52 +59,34 @@ export const parse = <T extends Node>(
     children: []
   })
 
-  let currentParent: T = null
-  let prevNode: T = root
+  let currentParent = root
+  let prevNode: T = null
   let currentTab = 0
 
   for (const line of lines) {
     const name = line.trim()
     if (!name) continue
     const tab = countWS(line)
+    const node = mapper({ name })
 
     if (tab > currentTab) {
       currentTab = tab
       currentParent = prevNode
     }
 
-    const node = mapper({ name })
-    if (tab === 0) {
-      root.children.push(node)
-      currentTab = 0
-    } else if (tab === currentTab) {
+    if (tab === currentTab) {
       const children = currentParent.children || []
       children.push(node)
       currentParent.children = children
-      Object.defineProperty(node, "parent", {
-        value: currentParent,
-        enumerable: false
-      })
-      Object.defineProperty(node, "tab", {
-        value: tab,
-        enumerable: false
-      })
+      setMeta(node, ["parent", currentParent], ["tab", tab])
     } else if (tab < currentTab) {
       const parent = getParent(currentParent, tab) || root
       parent.children.push(node)
+      currentTab = tab
     }
 
     prevNode = node
   }
 
   return root
-}
-
-const getParent = (node: Node, tab: number) => {
-  let nodeTab = node.tab
-  while (nodeTab >= tab) {
-    nodeTab = node.tab
-    node = node.parent
-  }
-  return node
 }
